4d7311eb3768f61d8d98e7249391f380f037bd8f
tyomashin <negichou@gmail.com>
2019-11-25 01:34:28 +0900

    no message

---
+++ PushNotificationSampleAndroid/app/src/main/java/com/tyomashin/pushnotificationsampleandroid/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.tyomashin.pushnotificationsampleandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Log.d("hoge",intent.extras["localNotification"]!!.toString())
+
+        button1.setOnClickListener {
+            // 通知を発行する
+            PushNotificationManager(this).showLocalNotification("title", "description")
+        }
+    }
+}
---
+++ PushNotificationSampleAndroid/app/src/main/java/com/tyomashin/pushnotificationsampleandroid/PushNotificationManager.kt
@@ -0,0 +1,62 @@
+package com.tyomashin.pushnotificationsampleandroid
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.NotificationCompat
+
+class PushNotificationManager(val context : Context){
+    companion object {
+        private const val CHANNEL_ID = "CHANNEL_ID"
+        private const val CHANNEL_NAME = "CHANNEL_NAME"
+        private const val CHANNEL_DESCRIPTION = "CHANNEL_DESCRIPTION"
+    }
+
+    // ローカル通知を発行する
+    fun showLocalNotification(title : String, description : String){
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(manager)
+
+        val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(description)
+
+        createPendingIntent()?.let {
+            notificationBuilder.setContentIntent(it)
+        }
+        val notification = notificationBuilder.build()
+        notification.flags = Notification.DEFAULT_LIGHTS or Notification.FLAG_AUTO_CANCEL
+        manager.notify(0, notification)
+    }
+
+    // チャネルを作成する
+    // 参考：https://developer.android.com/training/notify-user/channels?hl=ja
+    private fun createNotificationChannel(manager: NotificationManager) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            if (manager.getNotificationChannel(CHANNEL_ID) == null) {
+                val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+                channel.description = CHANNEL_DESCRIPTION
+                manager.createNotificationChannel(channel)
+            }
+        }
+    }
+    // 通知タップ時に動作させるためにPendingIntentを作成する
+    // 参考：https://joyplot.com/documents/2018/04/27/kotlin-android-notification/
+    // https://developer.android.com/training/notify-user/navigation#kotlin
+    // https://qiita.com/SnowMonkey/items/7cdc52c83bb9490d97a5
+    // 考察すること：https://codeday.me/jp/qa/20181210/56536.html
+    private fun createPendingIntent() : PendingIntent?{
+        val resultIntent = Intent(context,MainActivity::class.java)
+        resultIntent.putExtra("localNotification","0")
+        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(context).run {
+            // Add the intent, which inflates the back stack
+            addNextIntentWithParentStack(resultIntent)
+            // Get the PendingIntent containing the entire back stack
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+        return resultPendingIntent
+    }
+
+
+}
